255c754c7c802dad88105fe925ffa242223f6bcd
Omkar Potdar <omkarpotdar97@gmail.com>
2021-05-14 22:49:24 +0530

    added tuple_tutorial.py

---
+++ tuple_tutorial.py
@@ -0,0 +1,8 @@
+tup = (21,36,14,25)
+print(tup) #in list we can change the values, but in tuple we cannot change the values.
+print(tup[3]) #  []means list, () means tuple.
+print(tup[1]) # [1] means element at position 1.
+print(tup.count(14)) #no. of times the element is occur.
+print(tup.index(14)) #position of element
+tup.count(0)# set is almost same as list, diff is it would not maintain the sequence and it will not support duplicate values.
+
