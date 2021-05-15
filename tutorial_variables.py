f48029781d6df585a3508339b8ef03491b5bb0f7
Omkar Potdar <omkarpotdar97@gmail.com>
2021-05-15 18:43:42 +0530

    tutorial_variables.py

---
+++ tutorial_variables.py
@@ -0,0 +1,17 @@
+num = 5
+print(id(num))
+name = 'omkar'
+print(id(name))
+a = 10
+b = a
+print(a)
+print(b)
+print(id(a))
+print(id(b))
+a = 9
+print(id(a))
+print(id(b))
+k = a
+print(id(k))
+b = 8
+print(id(b))
