533b2791c7ece474c9493813ebfecc77413e83fb
Omkar Potdar <omkarpotdar97@gmail.com>
2021-05-25 15:37:32 +0530

    operators_in_python.py git commit -m operators_in_python.py

---
+++ operators_in_python.py
@@ -0,0 +1,39 @@
+x = 2
+y = 3
+print(x+y) # These are the Arithmatic operators. Additio.
+print(x-y) # Subtstraction.
+print(x*y) # Multiplication.
+print(x/y) # Division.
+x = x+2
+print(x)
+x += 2   # Enhancing the values by itself.
+print(x)
+x *= 3 # Here x is 6 and multiply itself by 3.
+print(x)
+a,b = 5,6 # This is Assignment operators.
+print(a)
+print(b)
+n = 7 # Uniary Operator.
+print(n)
+print(-n)
+n = -n
+print(n)
+print(a < b) # This is Relational Operators.
+print(a>b)
+print(a==b)
+a = 6
+print(a==b) # two times '==' symbol is use to comparing the values.
+print(a<=b) # less than equal to.
+print(a>=b) # Greater than equal to.
+print(a!=b) # '!' is used for 'not' so this is not equal to.
+a = 5 # Logical Operator.
+b = 4
+print(a<8 and b<5) # In 'AND' logic If both values are true then output is is true.
+print(a<8 and b<2)
+print(a<8 or b<2) # In 'Or' logic atleast one value is true then output is true.
+x = True
+print(x)
+print(not x) # In 'not' logic reverse the operation.
+x = not x
+print(x)
+
